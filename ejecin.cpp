b27615378ea917e03a4bd0c104e4f676596864ca
Danny <dannymg2002@gmail.com>
2017-06-25 08:31:58 -0500

    Ejemplo de cin

---
+++ ejecin.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,a;
+    cout<<"Ingrese un numero: ";  
+    cin>>x;
+    a= x*x*x;
+    cout<<"Respuesta: ";
+    cout<<a; 
+}
---
+++ ppc.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    float x;
+    cout<<"numero 1: ";
+    cin>>a;
+    cout<<"numero 2: ";
+    cin>>b;
+    x=a+b;
+    cout<<"respuesta: ";
+    cout<<x;
+}
